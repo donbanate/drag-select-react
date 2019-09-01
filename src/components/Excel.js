a2ed983f326b72fb64959dbb60dd77ccd2a98b8f
donbanate <lbanate97@gmail.com>
2019-09-01 11:03:14 +0800

    Using react-router

---
+++ src/components/Excel.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Excel extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sheets goes here..</h1>
+      </div>
+    );
+  }
+}
+
+export default Excel;
---
+++ src/components/Model.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Model extends Component {
+  render() {
+    return (
+      <div>
+        <code>Samples</code>
+      </div>
+    );
+  }
+}
+
+export default Model;
